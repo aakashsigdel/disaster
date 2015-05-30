99c2733bcb63e36ed6596dcc2995eea77c3e5f55
Rup Shri <irhspur@gmail.com>
2015-05-30 09:52:29 +0545

    Added angular code

--- js/lib/controller.js
+++ js/lib/controller.js
@@ -1,4 +1,15 @@
 angular.module('AssesmentApp')
 	.controller('reportCtrl', ['$scope', 'Service', function($scope, Service) {
+		$scope.getAssesmentDatum = function(assesmentId) {
+			return Service.getAssesmentDatum(assesmentId);
+		}
 
+		$scope.getAssesmentData = function() {
+			return Service.getAssesmentData();
+		}
+	}]),
+	controller('assesmentListCtrl' ['$scope', 'Service', function($scope, Service) {
+		$scope.getAssesmentData = function() {
+			return Service.getAssesmentData();
+		}
 	}])
